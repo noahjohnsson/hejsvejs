e5a867c6eea88943ab89b1117f4ac71b80e36da5
Philipsjoberg <118897693+Philipsjoberg@users.noreply.github.com>
2024-02-27 20:07:38 +0100

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,8 @@
-
 public class Application {
     public static void main(String[] args) {
-        Model vehicleModel = new Model();
-        View vehicleView = new View("CarSim 1.0", vehicleModel);
-        Controller vehicleController = new Controller(vehicleModel, vehicleView);
-        vehicleController.getTimer().start();
-
+        Model vehicleModel = new Model(); // Instantiate the Model
+        View vehicleView = new View("CarSim 1.0", vehicleModel); // Instantiate the View
+        Controller vehicleController = new Controller(vehicleModel, vehicleView); // Instantiate the Controller
+        vehicleModel.addObserver(vehicleView);
     }
-
 }
